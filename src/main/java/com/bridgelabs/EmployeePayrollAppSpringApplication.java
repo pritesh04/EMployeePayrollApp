a40642190ff1a54d8497197e3cc0152d6c1c7340
MSDOMAIN1\pbhanga <priteshraj.bhangale@morningstar.com>
2021-09-30 17:28:31 +0530

    [Pritesh] Initial setup

---
+++ src/main/java/com/bridgelabs/EmployeePayrollAppSpringApplication.java
@@ -0,0 +1,13 @@
+package com.bridgelabs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeePayrollAppSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeePayrollAppSpringApplication.class, args);
+	}
+
+}
